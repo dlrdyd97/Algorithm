6ae67d2f36771d25997f2dd80d07181c6f42266d
Kim IkHwan <121559376+dlrdyd97@users.noreply.github.com>
2023-09-12 00:55:03 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.23 ms, Memory: 69.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+		for (int i = 0; i < n; i++) {
+			String a = String.format("%16s", Integer.toBinaryString(arr1[i] | arr2[i]));
+			a = a.substring(16 - n);
+			a = a.replace("1", "#");
+			a = a.replace("0", " ");
+            answer[i] = a;
+		}
+        return answer;
+    }
+}
