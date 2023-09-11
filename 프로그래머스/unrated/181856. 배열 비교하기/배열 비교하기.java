2efe372199420aaac877abb028a4bac27bd49439
Kim IkHwan <121559376+dlrdyd97@users.noreply.github.com>
2023-09-12 00:46:10 +0900

    [unrated] Title: 배열 비교하기, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        int a = 0;
+		int b = 0;
+		if (arr1.length > arr2.length) {
+			answer = 1;
+		}
+		if (arr1.length < arr2.length) {
+			answer = -1;
+		}
+		if (arr1.length == arr2.length) {
+			for (int i = 0; i < arr1.length; i++) {
+				a += arr1[i];
+				b += arr2[i];
+			}
+			if (a > b) {
+				answer = 1;
+			}
+			if (a < b) {
+				answer = -1;
+			}
+			if (a == b) {
+				answer = 0;
+			}
+		}
+        return answer;
+    }
+}
