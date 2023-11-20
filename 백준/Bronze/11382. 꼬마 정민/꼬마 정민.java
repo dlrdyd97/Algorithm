f77e2f869f2963e251aa753fcebbed425c3f60f3
Kim IkHwan <121559376+dlrdyd97@users.noreply.github.com>
2023-11-20 18:59:31 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        long sum = 0;
+        while (st.hasMoreTokens()){
+            sum += Long.parseLong(st.nextToken());
+        }
+
+        System.out.println(sum);
+		
+	}
+
+}
