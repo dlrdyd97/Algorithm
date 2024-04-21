07a14c2dc3dd728512c6a7e8ec225ae964a5d153
Kim IkHwan <121559376+dlrdyd97@users.noreply.github.com>
2024-04-21 14:17:42 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.17 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,32 +1,29 @@
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
-		HashMap<String, HashSet<String>> map = new HashMap<>();
-		HashMap<String, Integer> idx = new HashMap<>();
+        HashMap<String, HashSet<String>> hm = new HashMap<>();
+        HashMap<String, Integer> idx = new HashMap<>();
 
-		for (int i = 0; i < id_list.length; i++) {
-			String name = id_list[i];
-			map.put(name, new HashSet<>());
-			idx.put(name, i);
-		}
+        for (int i = 0; i < id_list.length; i++) {
+            idx.put(id_list[i], i);
+            hm.put(id_list[i], new HashSet<>());
+        }
 
-		for (int i = 0; i < report.length; i++) {
-			String[] spl = report[i].split(" ");
-			String A = spl[0];
-			String B = spl[1];
-			map.get(B).add(A);
-		}
+        for (int i = 0; i < report.length; i++) {
+            String a = report[i].split(" ")[0]; // 신고자
+            String b = report[i].split(" ")[1]; // 피신고자
+            hm.get(b).add(a);
+        }
 
-		for (int i = 0; i < map.size(); i++) {
-			if (map.get(id_list[i]).size() >= k) {
-				for (String name : map.get(id_list[i])) {
-					answer[idx.get(name)]++;
-				}
-			}
-		}
+        for (String key : hm.keySet()) {
+            if (hm.get(key).size() >= k) {
+                for (String hsKey : hm.get(key)) {
+                    answer[idx.get(hsKey)]++;
+                }
+            }
+        }
         return answer;
     }
 }
