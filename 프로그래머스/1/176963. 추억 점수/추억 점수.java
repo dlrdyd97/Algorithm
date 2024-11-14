262ee33b231eabad1353c097336ef07b36254a7a
Kim IkHwan <121559376+dlrdyd97@users.noreply.github.com>
2024-11-14 17:57:08 +0900

    [level 1] Title: 추억 점수, Time: 16.13 ms, Memory: 103 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) { 
+        int[] answer = new int[photo.length];
+
+		for (int i = 0; i < photo.length; i++) {
+			ArrayList<String> fdzz = new ArrayList<>();
+			for (int j = 0; j < photo[i].length; j++) {
+				fdzz.add(photo[i][j]);
+			}
+			for (int j = 0; j < name.length; j++) {
+				if (fdzz.contains(name[j])) {
+					answer[i] += yearning[j];
+				}
+			}
+		}
+        return answer;
+    }
+}
